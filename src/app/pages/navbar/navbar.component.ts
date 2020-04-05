30217e18255d7df8e6bb49124503be7a346011a7
Tarnn <taranjitk18@gmail.com>
2020-04-05 18:09:19 -0400

    About Skills and pic

--- src/app/pages/navbar/navbar.component.ts
+++ src/app/pages/navbar/navbar.component.ts
@@ -1,22 +1,36 @@
-import { Router } from '@angular/router';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { Router, ActivatedRoute } from '@angular/router';
 import { RESUME_LINK, RESUME_ROUTE } from './../../app.constants';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
+  private routeSubscription: Subscription;
   isNavbarCollapsed = true;
   
-  constructor(private router: Router) { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
 
   ngOnInit(): void {
   }
 
   goToResumePage(): void {
     this.router.navigate([RESUME_ROUTE]);
+  }
+
+  anchorClick(): void {
+   this.routeSubscription = this.route.fragment.subscribe(f => {
+      const element = document.querySelector ( "#" + f )
+      if (element) element.scrollIntoView(true);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.routeSubscription.unsubscribe();
   }
 
 }
