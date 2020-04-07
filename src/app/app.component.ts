dd51c07c1d177f623b8e39b9e35270c23878ca77
Tarnn <taranjitk18@gmail.com>
2020-04-07 00:23:38 -0400

    updated logo + navbar logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ export class AppComponent implements OnInit, OnDestroy{
 
     setTimeout(() => {
       this.isLoading = false;
-    }, 2400);
+    }, 2500);
 
     this.routerSubscription = this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
--- src/app/misc/loader/loader.component.ts
+++ src/app/misc/loader/loader.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { fadeInOnEnterAnimation, fadeOutOnLeaveAnimation } from 'angular-animations';
 
 @Component({
